02fc02d69a7d6167c22387859342da49869e1178
Matt Binsfeld <mattb@windward.local>
2016-02-19 14:48:59 -0700

    posted license in all files

--- ReportPptx.rb
+++ ReportPptx.rb
@@ -1,3 +1,8 @@
+# "THE BEER-WARE LICENSE" 
+# As long as you retain this notice you can do whatever you want with this 
+# stuff. If you meet an employee from Windward some day, and you think this
+# stuff is worth it, you can buy them a beer in return. Windward Studios
+
 require_relative 'report'
 class ReportPptx < Report
 	def initialize uri, template, report = nil
--- ReportRtf.rb
+++ ReportRtf.rb
@@ -1,3 +1,9 @@
+# "THE BEER-WARE LICENSE" 
+# As long as you retain this notice you can do whatever you want with this 
+# stuff. If you meet an employee from Windward some day, and you think this
+# stuff is worth it, you can buy them a beer in return. Windward Studios
+
+
 require_relative 'report'
 class ReportRtf < Report
 	def initialize uri, template, report = nil
